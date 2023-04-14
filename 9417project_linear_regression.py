ff8da6e4424d87a441bcf4945828a2cd20ab372c
Ji-Zhan <74079044+Ji-Zhan@users.noreply.github.com>
2023-04-14 14:32:42 +1000

    Update 9417project_linear_regression.py

--- 9417project_linear_regression.py
+++ 9417project_linear_regression.py
@@ -1,151 +1,90 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
-
-
-# In[2]:
-
-
-train = pd.read_csv('C:\\Users\\Neo Zhan\\Desktop\\ashrae-energy-prediction\\train.csv')
-building_metadata = pd.read_csv('C:\\Users\\Neo Zhan\\Desktop\\ashrae-energy-prediction\\building_metadata.csv')
-
-
-# In[3]:
-
-
-weather_train = pd.read_csv('C:\\Users\\Neo Zhan\\Desktop\\ashrae-energy-prediction\\weather_train.csv')
-
-
-# In[4]:
-
-
-merged_df = pd.merge(weather_train, building_metadata, on='site_id', how='inner')
-
-
-# In[5]:
-
-
-Final_merged_df = pd.merge(merged_df, train, on=['building_id', 'timestamp'], how='inner')
-
-
-# In[6]:
-
-
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.metrics import mean_squared_error
 
+data = pd.read_csv('mini_train.csv')
 
-# In[7]:
+data["hour"] = pd.to_datetime(data['timestamp']).dt.hour
+data["weekday"] = pd.to_datetime(data['timestamp']).dt.weekday
+data = data.drop("timestamp", axis=1)
 
-
-Final_merged_df['timestamp'] = pd.to_datetime(Final_merged_df['timestamp'])
-
-
-# In[8]:
+data_update = data.dropna(thresh=len(data)*0.6, axis=1)
 
 
 from sklearn.preprocessing import LabelEncoder
 encoder = LabelEncoder()
+encoder.fit(data_update['primary_use'])
 
 
-# In[9]:
+encoded_col = encoder.transform(data_update['primary_use'])
+data_update['primary_use'] = encoded_col
 
 
-encoder.fit(Final_merged_df['primary_use'])
 
 
-# In[10]:
+
+# Remove outliers
+Q1 = data_update.quantile(0.25)
+Q3 = data_update.quantile(0.75)
+IQR = Q3 - Q1
+data_cleaned = data_update[~((data_update < (Q1 - 1.5 * IQR)) | (data_update > (Q3 + 1.5 * IQR))).any(axis=1)]
 
 
-encoded_col = encoder.transform(Final_merged_df['primary_use'])
 
 
-# In[11]:
+X = data_cleaned.drop(columns = ['meter_reading'])
 
 
-Final_merged_df['primary_use'] = encoded_col
 
 
-# In[12]:
+Y= data_cleaned[['meter_reading']]
 
 
-imp = IterativeImputer(max_iter=10, random_state=0)
 
 
-# In[13]:
+#Fill in the null values with the most frequent values in column 
+X_filled = X.fillna(X.mode().iloc[0])
 
 
-import numpy as np
-epoch = np.datetime64('1970-01-01T00:00:00')
 
 
-# In[14]:
+X_filled_train, X_filled_test, Y_train, Y_test = train_test_split(X_filled, Y, test_size=0.2, random_state=42)
 
 
-float_array = (Final_merged_df['timestamp'] - epoch) / np.timedelta64(1, 's')
 
 
-# In[15]:
+from sklearn.preprocessing import StandardScaler
+# Create a StandardScaler object
+scaler = StandardScaler()
+X_filled_train_scaled = scaler.fit_transform(X_filled_train)
+X_filled_test_scaled = scaler.transform(X_filled_test)
 
 
-Final_merged_df['timestamp']=float_array
 
 
-# In[16]:
+model = LinearRegression()
+# Train the model on the training data
+model.fit(X_filled_train_scaled, Y_train)
+# Use the model to make predictions on the testing data
+y_pred = model.predict(X_filled_test_scaled)
 
 
-Final_merged_df_sample = Final_merged_df.sample(frac=0.2)
 
 
-# In[17]:
+from sklearn.metrics import mean_squared_error
+mse = mean_squared_error(Y_test, y_pred)
+print('Mean squared error:', mse)
+r2 = r2_score(Y_test, y_pred)
+print('R-squared:', r2)
+rmse = mean_squared_error(Y_test, y_pred, squared=False)
+print('Root mean squared error:', rmse)
 
 
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score
 
 
-# In[18]:
-
-
-X = Final_merged_df_sample[['timestamp', 'air_temperature', 'cloud_coverage', 'dew_temperature', 'precip_depth_1_hr', 'sea_level_pressure', 'wind_direction', 'wind_speed', 'primary_use', 'square_feet','year_built', 'floor_count', 'meter']]
-
-
-# In[19]:
-
-
-y = Final_merged_df_sample['meter_reading']
-
-
-# In[20]:
-
-
-# Initialize the imputer MICE
-imputer = IterativeImputer()
-
-# Impute the missing values
-X_imputed = pd.DataFrame(imputer.fit_transform(X))
-
-
-# In[21]:
-
-
-X_imputed.columns = X.columns
-
-
-# In[22]:
-
-
-X_imputed['primary_use'] = X_imputed['primary_use'].astype(int)
-X_imputed['square_feet'] = X_imputed['square_feet'].astype('int64')
-X_imputed['meter'] = X_imputed['meter'].astype('int64')
-
-
-# In[23]:
-
-
-X_imputed_train, X_imputed_test, y_train, y_test = train_test_split(X_imputed, y, test_size=0.2, random_state=42)
